fe3b703b7ccaf060fb492613a5b3ab1eff8955d1
Ssnnaaiill <newdk3025@gmail.com>
2019-11-20 09:13:13 +0900

    🎉 Initial commit

---
+++ chat-front/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid fill-height aacontainer>
+        <router-view />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss"></style>
